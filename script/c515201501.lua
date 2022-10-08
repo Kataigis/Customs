b9a25f7f8fa41d7a745a89ed952305864b873f7c
Kataigis <111102572+Kataigis@users.noreply.github.com>
2022-10-08 01:20:04 -0400

    Update c515201501.lua

--- script/c515201501.lua
+++ script/c515201501.lua
@@ -83,7 +83,7 @@ function s.thop(e,tp,eg,ep,ev,re,r,rp)
 	if c:IsFaceup() and c:IsRelateToEffect(e) then
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_CHANGE_LEVEL)
+		e1:SetCode(EFFECT_UPDATE_LEVEL)
 		e1:SetValue(e:GetLabel())
 		e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE+RESET_PHASE+PHASE_END)
 		c:RegisterEffect(e1)
