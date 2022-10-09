0891ef32ba4239d724594a68f11b02abfe5cf5ad
Kataigis <111102572+Kataigis@users.noreply.github.com>
2022-10-08 21:35:51 -0400

    Update c515201501.lua

--- script/c515201501.lua
+++ script/c515201501.lua
@@ -23,14 +23,14 @@ function s.initial_effect(c)
 	e2:SetOperation(s.thop)
 	c:RegisterEffect(e2)
 	--Tuner
-	--local e3=Effect.CreateEffect(c)
-	--e3:SetType(EFFECT_TYPE_SINGLE)
-	--e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-	--e3:SetRange(LOCATION_MZONE)
-	--e3:SetCode(EFFECT_ADD_TYPE)
-	--e3:SetCondition(s.ntval)
-	--e3:SetValue(TYPE_TUNER)
-	--c:RegisterEffect(e3)
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCode(EFFECT_ADD_TYPE)
+	e3:SetValue(TYPE_TUNER, s.ntval)
+	e3:SetReset(RESET_EVENT+RESETS_STANDARD)
+	c:RegisterEffect(e3)
 end
 s.listed_series={0x2016}
 s.listed_names={id}
@@ -101,6 +101,6 @@ function s.thop(e,tp,eg,ep,ev,re,r,rp)
 		end
 	end
 end
---function s.ntval(c,sc,tp)
-	--return sc and sc:IsAttribute(ATTRIBUTE_WIND)
---end
+function s.ntval(c,sc,tp)
+	return sc and sc:IsAttribute(ATTRIBUTE_WIND)
+end
