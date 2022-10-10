c6e8aee08d454e651115405e157ef142590841d0
Kataigis <111102572+Kataigis@users.noreply.github.com>
2022-10-10 11:45:42 -0400

    Update c719202116.lua

--- script/c719202116.lua
+++ script/c719202116.lua
@@ -5,7 +5,106 @@ function s.initial_effect(c)
 	--Link summon method
 	c:EnableReviveLimit()
 	Link.AddProcedure(c,s.matfilter,1,1)
+	--Banish recycle
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetCountLimit(1,id)
+	e1:SetTarget(s.bntgt)
+	e1:SetOperation(s.bnop)
+	c:RegisterEffect(e1)
+	--Synchro Summon
+	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_REMOVE+CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetCountLimit(1,{id,1})
+	e2:SetTarget(s.sptgt)
+	e2:SetOperation(s.spact)
+	e2:SetHintTiming(0,TIMING_END_PHASE)
+	c:RegisterEffect(e2)
 end
+s.listed_series={0x917}
 function s.matfilter(c,lc,stype,tp)
 	return c:IsSetCard(0x917,lc,stype,tp) and not c:IsType(TYPE_LINK,lc,stype,tp)
+end
+
+function s.tgfilter(c)
+	return c:IsSetCard(0x917) and c:IsFaceup()
+end
+function s.bntgt(e,tp,eg,ep,ev,re,r,rp,chk)
+	--local c=e:GetHandler()
+	if chk==0 then return Duel.IsExistingMatchingCard(s.tgfilter,tp,LOCATION_REMOVED,0,1,nil) end
+	local g=Duel.GetMatchingGroup(s.tgfilter,tp,LOCATION_REMOVED,0,nil)
+	e:SetCategory(CATEGORY_TOGRAVE)
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,g,1,0,0)
+end
+function s.bnop(e,tp,eg,ep,ev,re,r,rp)
+	--local c=e:GetHandler()
+	if Duel.IsExistingMatchingCard(s.tgfilter,tp,LOCATION_REMOVED,0,1,nil) then
+		local tg=Duel.SelectMatchingCard(tp,s.tgfilter,tp,LOCATION_REMOVED,0,1,1,nil)
+		if #tg>0 then
+			Duel.SendtoGrave(tg,REASON_EFFECT+REASON_RETURN)
+		end
+	end
+end
+
+function s.spfilter1(c,e,tp)
+	local lv=c:GetLevel()
+	return c:IsSetCard(0x917) and c:IsType(TYPE_SYNCHRO) and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_SYNCHRO,tp,false,false)
+		and Duel.IsExistingMatchingCard(s.spfilter2,tp,LOCATION_MZONE+LOCATION_GRAVE,0,1,nil,tp,c)
+end
+function s.rescon(tuner,scard)
+	return	function(sg,e,tp,mg)
+				sg:AddCard(tuner)
+				local res=Duel.GetLocationCountFromEx(tp,tp,sg,scard)>0 
+					and sg:CheckWithSumEqual(Card.GetLevel,scard:GetLevel(),#sg,#sg)
+				sg:RemoveCard(tuner)
+				return res
+			end
+end
+function s.spfilter2(c,tp,sc)
+	local rg=Duel.GetMatchingGroup(s.spfilter3,tp,LOCATION_MZONE+LOCATION_GRAVE,0,c)
+	return c:IsType(TYPE_TUNER) and c:IsAbleToRemove() and aux.SpElimFilter(c,true) 
+		and aux.SelectUnselectGroup(rg,e,tp,nil,2,s.rescon(c,sc),0)
+end
+function s.spfilter3(c)
+	return c:HasLevel() and not c:IsType(TYPE_TUNER) and c:IsAbleToRemove() and aux.SpElimFilter(c,true)
+end
+function s.sptgt(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then
+		local pg=aux.GetMustBeMaterialGroup(tp,Group.CreateGroup(),tp,nil,nil,REASON_SYNCHRO)
+		return #pg<=0 and Duel.IsExistingMatchingCard(s.spfilter1,tp,LOCATION_EXTRA,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
+end
+function s.spact(e,tp,eg,ep,ev,re,r,rp)
+	local pg=aux.GetMustBeMaterialGroup(tp,Group.CreateGroup(),tp,nil,nil,REASON_SYNCHRO)
+	if #pg>0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g1=Duel.SelectMatchingCard(tp,s.spfilter1,tp,LOCATION_EXTRA,0,1,1,nil,e,tp)
+	local sc=g1:GetFirst()
+	if sc then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+		local g2=Duel.SelectMatchingCard(tp,s.spfilter2,tp,LOCATION_MZONE+LOCATION_GRAVE,0,1,1,nil,tp,sc)
+		local tuner=g2:GetFirst()
+		local rg=Duel.GetMatchingGroup(s.spfilter3,tp,LOCATION_MZONE+LOCATION_GRAVE,0,tuner)
+		local sg=aux.SelectUnselectGroup(rg,e,tp,1,2,s.rescon(tuner,sc),1,tp,HINTMSG_REMOVE,s.rescon(tuner,sc))
+		sg:AddCard(tuner)
+		Duel.Remove(sg,POS_FACEUP,REASON_EFFECT)
+		Duel.SpecialSummonStep(sc,SUMMON_TYPE_SYNCHRO,tp,tp,false,false,POS_FACEUP)
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_DISABLE)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		sc:RegisterEffect(e1,true)
+		local e2=Effect.CreateEffect(e:GetHandler())
+		e2:SetType(EFFECT_TYPE_SINGLE)
+		e2:SetCode(EFFECT_DISABLE_EFFECT)
+		e2:SetReset(RESET_EVENT+RESETS_STANDARD)
+		sc:RegisterEffect(e2,true)
+		sc:CompleteProcedure()
+	end
+	Duel.SpecialSummonComplete()
 end
