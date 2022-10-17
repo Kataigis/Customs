405b0d6def43713cb60fb4038e11eb69b3db0efa
Kataigis <111102572+Kataigis@users.noreply.github.com>
2022-10-16 22:01:19 -0400

    Update c719202117.lua

--- script/c719202117.lua
+++ script/c719202117.lua
@@ -4,7 +4,6 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--activate
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_LEAVE_FIELD)
 	e1:SetCountLimit(1,id)
