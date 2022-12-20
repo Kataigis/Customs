01c92f7f8c8f4cd0d5d33e03bec8ed65d85f98ef
Kataigis <111102572+Kataigis@users.noreply.github.com>
2022-12-20 00:17:57 -0500

    Update c105000002.lua

--- script/c105000002.lua
+++ script/c105000002.lua
@@ -66,6 +66,15 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if not c:IsRelateToEffect(e) then return end
 	if Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)~=0 then
-		
+		--You take no battle damage
+		local e1=Effect.CreateEffect(c)
+		e1:SetDescription(aux.Stringid(id,1))
+		e1:SetType(EFFECT_TYPE_FIELD)
+		e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
+		e1:SetCode(EFFECT_AVOID_BATTLE_DAMAGE)
+		e1:SetTargetRange(1,0)
+		e1:SetValue(1)
+		e1:SetReset(RESET_PHASE+PHASE_END)
+		Duel.RegisterEffect(e1,tp)
 	end
 end
