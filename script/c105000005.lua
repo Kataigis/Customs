bff28947cc6cb44ceeb9c5f2ba106c16e04158a9
Kataigis <111102572+Kataigis@users.noreply.github.com>
2022-12-19 23:14:41 -0500

    Update c105000005.lua

--- script/c105000005.lua
+++ script/c105000005.lua
@@ -21,7 +21,7 @@ function s.bnfilter(c)
 end
 function s.bncost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.bnfilter,tp,LOCATION_HAND|LOCATION_ONFIELD|LOCATION_GRAVE,0,1,nil) end
-	local maxtc=Duel.GetTargetCount(nil,tp,0,LOCATION_ONFIELD,nil)
+	local maxtc=Duel.GetTargetCount(nil,tp,0,LOCATION_MZONE,nil)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
 	local g=Duel.SelectMatchingCard(tp,s.bnfilter,tp,LOCATION_HAND|LOCATION_ONFIELD|LOCATION_GRAVE,0,1,maxtc,nil)
 	local cg=Duel.Remove(g,POS_FACEUP,REASON_COST)
@@ -29,14 +29,30 @@ function s.bncost(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function s.bntg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsOnField() and chkc:IsControler(1-tp) end
-	if chk==0 then return Duel.IsExistingTarget(nil,tp,0,LOCATION_ONFIELD,1,nil) end
+	if chk==0 then return Duel.IsExistingTarget(nil,tp,0,LOCATION_MZONE,1,nil) end
 	local ct=e:GetLabel()
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-	local g=Duel.SelectTarget(tp,nil,tp,0,LOCATION_ONFIELD,ct,ct,nil)
+	local g=Duel.SelectTarget(tp,nil,tp,0,LOCATION_MZONE,ct,ct,nil)
 	Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,ct,0,0)
 end
 function s.bnop(e,tp,eg,ep,ev,re,r,rp)
-	local tg=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
-	local rg=tg:Filter(Card.IsRelateToEffect,nil,e)
-	if #rg>0 then Duel.Remove(rg,REASON_EFFECT) end
+	
+	local tg=Duel.GetTargetCards(e)
+	if Duel.Remove(tg,0,REASON_EFFECT+REASON_TEMPORARY)>0 then
+		local c=e:GetHandler()
+		for rc in tg:Iter() do
+			local e1=Effect.CreateEffect(c)
+			e1:SetDescription(aux.Stringid(id,1))
+			e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+			e1:SetCode(EVENT_PHASE+PHASE_END)
+			e1:SetReset(RESET_PHASE+PHASE_END)
+			e1:SetLabelObject(rc)
+			e1:SetCountLimit(1)
+			e1:SetOperation(s.retop)
+			Duel.RegisterEffect(e1,tp)
+		end
+	end
+end
+function s.retop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.ReturnToField(e:GetLabelObject())
 end
