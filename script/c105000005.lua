a0232189c198a74ac3127f25cc572b8de7d3130d
Kataigis <111102572+Kataigis@users.noreply.github.com>
2022-12-19 22:48:51 -0500

    Create c105000005.lua

---
+++ script/c105000005.lua
@@ -0,0 +1,42 @@
+--Metaphys Displacement
+--Kataigis
+local s,id=GetID()
+function s.initial_effect(c)
+	--activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetCost(s.bncost)
+	e1:SetTarget(s.bntg)
+	e1:SetOperation(s.bnop)
+	c:RegisterEffect(e1)
+end
+s.listed_series={0x105}
+
+function s.bnfilter(c)
+	return c:IsSetCard(0x105) and c:IsAbleToRemoveAsCost()
+end
+function s.bncost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.bnfilter,tp,LOCATION_HAND+LOCATION_ONFIELD+LOCATION_GRAVE,0,1,nil) end
+	local maxtc=Duel.GetTargetCount(nil,tp,0,LOCATION_ONFIELD,nil)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g=Duel.SelectMatchingCard(tp,s.bnfilter,tp,LOCATION_HAND+LOCATION_ONFIELD+LOCATION_GRAVE,0,1,maxtc,nil)
+	local cg=Duel.Remove(g,POS_FACEUP,REASON_COST)
+	e:SetLabel(cg)
+end
+function s.bntg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsOnField() and chkc:IsControler(1-tp) end
+	if chk==0 then return Duel.IsExistingTarget(nil,tp,0,LOCATION_ONFIELD,1,nil) end
+	local ct=e:GetLabel()
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g=Duel.SelectTarget(tp,nil,tp,0,LOCATION_ONFIELD,ct,ct,nil)
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,ct,0,0)
+end
+function s.bnop(e,tp,eg,ep,ev,re,r,rp)
+	local tg=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
+	local rg=tg:Filter(Card.IsRelateToEffect,nil,e)
+	if #rg>0 then Duel.Remove(rg,REASON_EFFECT) end
+end
