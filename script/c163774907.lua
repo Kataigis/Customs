f218244c237bb401a7db7a45f03ca80a3c1e0ad9
Kataigis <111102572+Kataigis@users.noreply.github.com>
2023-01-21 18:36:56 -0500

    Update c163774907.lua

--- script/c163774907.lua
+++ script/c163774907.lua
@@ -1,2 +1,40 @@
 --Relaised Striker
 --Kataigis
+local s,id=GetID()
+function s.initial_effect(c)
+	--Fusion Summon procedure
+	c:EnableReviveLimit()
+	Fusion.AddProcFunRep(c,aux.FilterBoolFunctionEx(Card.IsSetCard,0x749),2,true)
+	--Search
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_REMOVE)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetCountLimit(1,{id,0})
+	e1:SetTarget(s.thtg)
+	e1:SetOperation(s.thop)
+	c:RegisterEffect(e1)
+	--Special Summon
+	
+end
+s.listed_series={0x749}
+function s.thfilter(c)
+	return c:IsAbleToHand() and (c:IsFaceup() or not c:IsLocation(LOCATION_EXTRA))
+		and (c:IsSetCard(0x749) and c:IsMonster()))
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,nil,1,tp,LOCATION_DECK)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+	local tg=g:GetFirst()
+	if tg==nil then return end
+	Duel.Remove(tg,POS_FACEUP,REASON_EFFECT)
+	Duel.BreakEffect()
+	local og=Duel.GetOperatedGroup()
+	Duel.SendtoHand(og,p,REASON_EFFECT)
+end
