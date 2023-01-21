5713530ab8df2b0fdb8844ab242501a3b7b5a57d
Kataigis <111102572+Kataigis@users.noreply.github.com>
2023-01-21 14:31:19 -0500

    Update c163712002.lua

--- script/c163712002.lua
+++ script/c163712002.lua
@@ -2,20 +2,23 @@
 --Kataigis
 local s,id=GetID()
 function s.initial_effect(c)
-	--spsummon
+	--Special Summon from the hand
 	local e4=Effect.CreateEffect(c)
 	e4:SetDescription(aux.Stringid(id,0))
 	e4:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e4:SetCode(EVENT_SUMMON_SUCCESS)
+	e4:SetProperty(EFFECT_FLAG_DELAY)
+	e4:SetCountLimit(1,id)
 	e4:SetTarget(s.sptg)
 	e4:SetOperation(s.spop)
 	c:RegisterEffect(e4)
-  local e5=e4:Clone()
-	e4:SetCode(EVENT_SPSUMMON_SUCCESS)
+	local e5=e4:Clone()
+	e5:SetCode(EVENT_SPSUMMON_SUCCESS)
 	c:RegisterEffect(e5)
 end
 s.listed_series={0x120}
+s.listed_names={id}
 function s.filter(c,e,tp)
 	return c:IsSetCard(0x120) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
@@ -26,7 +29,6 @@ function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
-	local c=e:GetHandler()
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
 	if #g>0 then
