c014ab94790c3655a02e0e795bdbd0d0249359b3
Kataigis <111102572+Kataigis@users.noreply.github.com>
2023-01-21 14:13:24 -0500

    Update c163712003.lua

--- script/c163712003.lua
+++ script/c163712003.lua
@@ -1,2 +1,34 @@
 --Relaised Carrier
 --Kataigis
+local s,id=GetID()
+function s.initial_effect(c)
+    --bounce
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,1))
+	e4:SetCategory(CATEGORY_TOHAND)
+	e4:SetType(EFFECT_TYPE_QUICK_O)
+	e4:SetCode(EVENT_FREE_CHAIN)
+	e4:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetCountLimit(1,id)
+	e4:SetTarget(s.thtg)
+	e4:SetOperation(s.thop)
+	c:RegisterEffect(e4)
+end
+s.listed_series={0x120}
+function s.thfilter(c)
+	return c:IsFaceup() and c:GetType()==TYPE_MONSTER and c:IsSetCard(0x120) and c:IsAbleToHand()
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.thfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.thfilter,tp,LOCATION_MZONE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
+	local g=Duel.SelectTarget(tp,s.thfilter,tp,LOCATION_MZONE,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsRelateToEffect(e) and Duel.SendtoHand(tc,nil,REASON_EFFECT)>0 then
+		local g=Duel.GetMatchingGroup(s.tffilter,tp,LOCATION_HAND,0,nil,tp)
+	end
+end
