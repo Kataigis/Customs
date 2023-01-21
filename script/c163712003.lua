67328392a506fea93c9be539e56a412d7e9a4a65
Kataigis <111102572+Kataigis@users.noreply.github.com>
2023-01-21 14:18:57 -0500

    Update c163712003.lua

--- script/c163712003.lua
+++ script/c163712003.lua
@@ -17,7 +17,7 @@ function s.initial_effect(c)
 end
 s.listed_series={0x120}
 function s.thfilter(c)
-	return c:IsFaceup() and c:GetType()==TYPE_MONSTER and c:IsSetCard(0x120) and c:IsAbleToHand()
+	return c:IsFaceup() and c:IsSetCard(0x120) and c:IsAbleToHand()
 end
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.thfilter(chkc) end
