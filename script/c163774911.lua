446a96bbd52c6c7abca429f090ffa1d2b223f471
Kataigis <111102572+Kataigis@users.noreply.github.com>
2023-01-22 01:19:55 -0500

    Update c163774911.lua

--- script/c163774911.lua
+++ script/c163774911.lua
@@ -4,8 +4,18 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
 	local e1=Fusion.CreateSummonEff(c,aux.FilterBoolFunction(Card.IsSetCard,0x749),aux.FALSE,s.fextra,Fusion.ShuffleMaterial,nil,nil,nil,nil,nil,nil,nil,nil,nil,s.extratg)
+	e1:SetCode(EVENT_TO_HAND)
 	e1:SetCountLimit(1,id)
+	e1:SetCondition(s.condition)
 	c:RegisterEffect(e1)
+end
+s.listed_series={0x749}
+function s.filter(c,tp)
+	return c:IsSetCard(0x749) and (c:IsPreviousLocation(LOCATION_MZONE) or c:IsPreviousLocation(LOCATION_GRAVE) or c:IsPreviousLocation(LOCATION_REMOVED))
+		and c:IsType(TYPE_MONSTER)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.filter,1,nil,tp)
 end
 function s.fextra(e,tp,mg)
 	return Duel.GetMatchingGroup(aux.NecroValleyFilter(Fusion.IsMonsterFilter(Card.IsFaceup,Card.IsAbleToDeck)),tp,LOCATION_GRAVE+LOCATION_REMOVED,0,nil)
