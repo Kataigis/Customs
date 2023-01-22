6da1cabcc8950e70558d75347d599b91a9c3a42c
Kataigis <111102572+Kataigis@users.noreply.github.com>
2023-01-21 22:12:59 -0500

    Update c163774916.lua

--- script/c163774916.lua
+++ script/c163774916.lua
@@ -1,2 +1,40 @@
 --Relaised Sentinel
 --Kataigis
+local s,id=GetID()
+function s.initial_effect(c)
+	--Fusion Summon procedure
+	c:EnableReviveLimit()
+	Fusion.AddProcFunRep(c,aux.FilterBoolFunctionEx(Card.IsSetCard,0x749),2,true)
+	--quick
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TOHAND)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1,id)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+	--recover
+	
+end
+s.listed_series={0x749}
+function s.thfilter(c)
+	return c:IsFaceup() and c:IsSetCard(0x749) and c:IsAbleToHand()
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return false end
+	if chk==0 then return Duel.IsExistingTarget(s.qfilter,tp,LOCATION_ONFIELD,0,1,e:GetHandler())
+		and Duel.IsExistingTarget(aux.TRUE,tp,0,LOCATION_ONFIELD,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOHAND)
+	local g1=Duel.SelectTarget(tp,s.qfilter,tp,LOCATION_ONFIELD,0,1,1,e:GetHandler())
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOHAND)
+	local g2=Duel.SelectTarget(tp,aux.TRUE,tp,0,LOCATION_ONFIELD,1,1,nil)
+	g1:Merge(g2)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g1,2,0,0)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetTargetCards(e)
+	Duel.SendToHand(g,nil,REASON_EFFECT)
+end
