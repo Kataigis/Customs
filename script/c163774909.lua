f15bd88b023aff8d50d23449b0362aed88a00320
Kataigis <111102572+Kataigis@users.noreply.github.com>
2023-01-21 22:55:01 -0500

    Update c163774909.lua

--- script/c163774909.lua
+++ script/c163774909.lua
@@ -1,2 +1,37 @@
 --Relaised Overlord
 --Kataigis
+local s,id=GetID()
+function s.initial_effect(c)
+	c:SetUniqueOnField(1,0,id)
+	--cannot be returned to the extra
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_CANNOT_TO_DECK)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetRange(LOCATION_MZONE)
+	c:RegisterEffect(e1)
+	--indestructible
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCondition(s.indcon)
+	e2:SetValue(s.indval)
+	c:RegisterEffect(e2)
+	--attack magnet
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetCode(EFFECT_CANNOT_SELECT_BATTLE_TARGET)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetTargetRange(0,LOCATION_MZONE)
+	e3:SetValue(s.atlimit)
+	c:RegisterEffect(e3)
+	--special summon
+end
+function s.indval(e,re,tp)
+	return tp~=e:GetHandlerPlayer()
+end
+function s.atlimit(e,c)
+	return c~=e:GetHandler()
+end
